78ca7df96ea7478f992902c4cbd1e371b6cc765a
Archishmaan <archishou@gmail.com>
2026-01-07 19:33:20 -0500

    Initial Commit

---
+++ Logd/LogdApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogdApp.swift
+//  Logd
+//
+//  Created by Archishmaan Peyyety on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LogdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
